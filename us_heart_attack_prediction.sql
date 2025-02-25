dade4b53def8f3465fd3273a8114074d56f4a0b7
Asad Ali <67062577+theasadali@users.noreply.github.com>
2025-02-25 23:20:54 +0500

    Added percentage of smokers and diabetes

--- us_heart_attack_prediction.sql
+++ us_heart_attack_prediction.sql
@@ -1,5 +1,6 @@
-use heart_attack_ds;
+use heart_attack_ds
 
+--Find avg and group by age and and gender
 select 
 AVG(Cholesterol) as avg_cholestrol, 
 AVG(BloodPressure) as avg_bp, 
@@ -7,5 +8,17 @@ ROUND(AVG(BMI),2) as avg_BMI, Age, Gender
 from heart_attack_dataset
 group by Age, Gender
 order by age asc
+
+--percentage of smokers
+select
+round(sum(cast(Smoker as float)) / count(Smoker) * 100, 2)
+as Smoker_Percentage from heart_attack_dataset;
+
+--percentage of diabetics
+select
+round(sum(cast(Diabetes as float)) / count(Diabetes) * 100, 2)
+as Diabetes_Percentage from heart_attack_dataset;
+
+
 
 
