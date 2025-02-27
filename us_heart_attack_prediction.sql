fc278a793f919811ffe81bb3f13cde22d666cd96
Asad Ali <67062577+theasadali@users.noreply.github.com>
2025-02-27 23:57:32 +0500

    Added query to heck if smokers have a higher probability of heart attacks than non-smokers

--- us_heart_attack_prediction.sql
+++ us_heart_attack_prediction.sql
@@ -2,9 +2,9 @@ use heart_attack_ds;
 
 --Find avg and group by age and and gender
 select 
-AVG(Cholesterol) as avg_cholestrol, 
-AVG(BloodPressure) as avg_bp, 
-ROUND(AVG(BMI),2) as avg_BMI, Age, Gender
+avg(Cholesterol) as avg_cholestrol, 
+avg(BloodPressure) as avg_bp, 
+round(avg(BMI),2) as avg_BMI, Age, Gender
 from heart_attack_dataset
 group by Age, Gender
 order by age asc
@@ -20,6 +20,7 @@ round(sum(cast(Diabetes as float)) / count(Diabetes) * 100, 2)
 as Diabetes_Percentage from heart_attack_dataset;
 
 --Query to find correlation between hypertension and heart-attack
+-- used Pearson's Correlation Cofficient (r)
 select 
     (count(*) * sum(cast(Hypertension as float) * cast(PreviousHeartAttack as float)) - sum(cast(Hypertension as float)) * sum(cast(PreviousHeartAttack as float))) /
     (sqrt((count(*) * sum(cast(Hypertension as float) * cast(Hypertension as float)) - power(sum(cast(Hypertension as float)), 2)) * 
@@ -33,3 +34,14 @@ from heart_attack_dataset
 where PreviousHeartAttack = 1
 group by Age
 order by number_of_HA_cases DESC
+
+--Query to heck if smokers have a higher probability of heart attacks than non-smokers
+-- P (smokers | heart attack) = number of smokers with heart attack / total number of smokers
+-- P (non-smokers | heart attack = number of non-smokers with heart attack / total number of smokers
+
+select Smoker,
+count(*) as total_people,
+sum(cast(PreviousHeartAttack as float)) as total_heart_attacks,
+sum(cast(PreviousHeartAttack as float)) * 100 / count(*) --checks the probability - group by clause seperates the smokers and non-smokers
+from heart_attack_dataset
+group by Smoker;
